38b0dfe88da065099efaadfd63f999c32adc4840
Dmitry Yu Okunev <dyokunev@ut.mephi.ru>
2014-09-05 18:38:08 +0400

    Hide top menu control buttons for guests

--- init.rb
+++ init.rb
@@ -12,9 +12,9 @@ Redmine::Plugin.register :redmine_topmenu_tasks do
 	version '0.0.1'
 	# Don't know how to get the Internatialization work
 	#menu :top_menu, :tasks_incoming, {:controller => 'issues', :action => 'index', :query_id => 10}, :caption => I18n.t(:tasks_incoming)
-	menu :top_menu, :tasks_incoming,  {:controller => 'issues', :action => 'index', :query_id => 10}, :caption => "Поручено мне",	:if => authorize_for('issues', 'new')
-	menu :top_menu, :tasks_outcoming, {:controller => 'issues', :action => 'index', :query_id => 56}, :caption => "Я поручил",	:if => authorize_for('issues', 'new')
-	menu :top_menu, :tasks_control,   {:controller => 'issues', :action => 'index', :query_id => 59}, :caption => "На контроле",	:if => authorize_for('issues', 'new')
-	menu :top_menu, :new_issue, { :controller => 'issues', :action => 'new', :copy_from => nil }, :caption => :label_issue_new,	:if => authorize_for('issues', 'new')
+	menu :top_menu, :tasks_incoming,  {:controller => 'issues', :action => 'index', :query_id => 10}, :caption => "Поручено мне",	:if => Proc.new { User.current.logged? }
+	menu :top_menu, :tasks_outcoming, {:controller => 'issues', :action => 'index', :query_id => 56}, :caption => "Я поручил",	:if => Proc.new { User.current.logged? }
+	menu :top_menu, :tasks_control,   {:controller => 'issues', :action => 'index', :query_id => 59}, :caption => "На контроле",	:if => Proc.new { User.current.logged? }
+	menu :top_menu, :new_issue, { :controller => 'issues', :action => 'new', :copy_from => nil }, :caption => :label_issue_new,	:if => Proc.new { User.current.logged? }
 end
 
